ef72accc9e1a8dd6ed4e42ce59746efebe219bf9
ketikai <ketikai@idealstate.team>
2026-04-19 16:06:37 +0800

    feat(ksu): 同步 'scope-minimized manual hooks v2.0' 更新。
    * 添加可选补丁 'policy_rwlock' 。
    * 为 'grus_defconfig' 显式配置 'CONFIG_KSU' 子项。
    * 为 'grus_defconfig' 显式留空 'CONFIG_LOCALVERSION' 值。
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- fs/exec.c
+++ fs/exec.c
@@ -1836,13 +1836,11 @@ out_ret:
 	putname(filename);
 	return retval;
 }
-
+ 
 #ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
 #endif
 
 int do_execve(struct filename *filename,
@@ -1852,10 +1850,7 @@ int do_execve(struct filename *filename,
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
-	else
-		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
@@ -1884,9 +1879,8 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-#ifdef CONFIG_KSU
-	if (!ksu_execveat_hook)
-		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL); /* 32-bit support */
+#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
